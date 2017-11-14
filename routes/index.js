8f89e4004f8475d50a6e75adf098d848f34196f3
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-11-14 18:07:48 +0100

    Added a new page, called index1 to see where I can make changes, as in the example on https://github.com/derekja/auth-db/

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index.html', { title: 'Cloudant Boiler Plate' });
 };
+
+exports.index = function(req, res){
+  res.render('index1.html', { title: 'index1' });
+};
